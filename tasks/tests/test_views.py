f814ca02dcaeb39d40fd9d7f5b077dbdb334a870
Serhii Horodilov <sgorodil@gmail.com>
2024-02-02 19:47:30 +0200

    Apply context for tasks views (GH-4)

--- tasks/tests/test_views.py
+++ tasks/tests/test_views.py
@@ -34,13 +34,18 @@ class TestTaskCreateView(test.TestCase):
 
 class TestTaskDetailView(test.TestCase):
     def setUp(self) -> None:
-        self.url_path = reverse("tasks:detail", args=(42,))
+        self.url_path = reverse("tasks:detail", args=(2,))
+        self.url_404 = reverse("tasks:detail", args=(42,))
         self.template_name = "task_detail.html"
         self.client = test.Client()
 
     def test_response_200(self):
         response = self.client.get(self.url_path)
         self.assertEqual(response.status_code, 200)
+
+    def test_response_404(self):
+        response = self.client.get(self.url_404)
+        self.assertEqual(response.status_code, 404)
 
     def test_template_used(self):
         response = self.client.get(self.url_path)
@@ -49,13 +54,18 @@ class TestTaskDetailView(test.TestCase):
 
 class TestTaskUpdateView(test.TestCase):
     def setUp(self) -> None:
-        self.url_path = reverse("tasks:update", args=(42,))
+        self.url_path = reverse("tasks:update", args=(2,))
+        self.url_404 = reverse("tasks:update", args=(42,))
         self.template_name = "task_form.html"
         self.client = test.Client()
 
     def test_response_200(self):
         response = self.client.get(self.url_path)
         self.assertEqual(response.status_code, 200)
+
+    def test_response_404(self):
+        response = self.client.get(self.url_404)
+        self.assertEqual(response.status_code, 404)
 
     def test_template_used(self):
         response = self.client.get(self.url_path)
@@ -64,8 +74,13 @@ class TestTaskUpdateView(test.TestCase):
 
 class TestTaskDeleteView(test.TestCase):
     def setUp(self) -> None:
-        self.url_path = reverse("tasks:delete", args=(42,))
+        self.url_path = reverse("tasks:delete", args=(2,))
+        self.url_404 = reverse("tasks:delete", args=(42,))
         self.client = test.Client()
+
+    def test_response_404(self):
+        response = self.client.get(self.url_404)
+        self.assertEqual(response.status_code, 404)
 
     def test_redirect(self):
         response = self.client.get(self.url_path)
--- tasks/views.py
+++ tasks/views.py
@@ -3,8 +3,47 @@ Tasks application views
 
 """
 
-from django.http import HttpRequest, HttpResponse
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
+
+tasks = [
+    {
+        "pk": 1,
+        "summary":
+            "Shield at the holodeck was the energy of assimilation, "
+            "lowered to a chemical c-beam.",
+        "description":
+            "The creature is more transformator now than starship. "
+            "Solid and always quirky. Energy at the wormhole was "
+            "the life of ellipse, grabbed to a mysterious proton. "
+            "Adventure at the space station was the flight of honor, "
+            "translated to a unrelated hurrah. Mermaids reproduce from "
+            "pressures like sub-light humour's?",
+        "assignee": {
+            "image": "https://i.pravatar.cc/?u=DoraHeadstrong@dayrep.com",
+            "first_name": "Dora",
+            "last_name": "Headstrong",
+        }
+    },
+    {
+        "pk": 2,
+        "summary":
+            "Sensor at the universe was the pattern of love, "
+            "empowered to a futile alien.",
+        "description":
+            "The nano machine is more admiral now than processor. "
+            "Senior and patiently dead. Pattern at the center was "
+            "the death of attitude, loved to a biological ship. "
+            "Sensor at the space station was the assimilation of "
+            "resistance, destroyed to an ancient crew. "
+            "The collective admiral finally manifests the planet?",
+        "assignee": {
+            "image": "https://i.pravatar.cc/?u=TobyMugwort@armyspy.com",
+            "first_name": "Toby",
+            "last_name": "Mugwort",
+        }
+    },
+]
 
 
 def task_list_view(request: HttpRequest) -> HttpResponse:
@@ -19,7 +58,11 @@ def task_list_view(request: HttpRequest) -> HttpResponse:
 
     """
 
-    return render(request, "task_list.html")
+    ctx = {
+        "object_list": tasks,
+    }
+
+    return render(request, "task_list.html", ctx)
 
 
 def task_create_view(request: HttpRequest) -> HttpResponse:
@@ -51,7 +94,17 @@ def task_detail_view(request: HttpRequest, pk: int) -> HttpResponse:
 
     """
 
-    return render(request, "task_detail.html")
+    for task in tasks:
+        if task["pk"] == pk:
+            break
+    else:
+        raise Http404
+
+    ctx = {
+        "object": task,
+    }
+
+    return render(request, "task_detail.html", ctx)
 
 
 def task_update_view(request: HttpRequest, pk: int) -> HttpResponse:
@@ -68,7 +121,17 @@ def task_update_view(request: HttpRequest, pk: int) -> HttpResponse:
 
     """
 
-    return render(request, "task_form.html")
+    for task in tasks:
+        if task["pk"] == pk:
+            break
+    else:
+        raise Http404
+
+    ctx = {
+        "object": task,
+    }
+
+    return render(request, "task_form.html", ctx)
 
 
 def task_delete_view(request: HttpRequest, pk: int) -> HttpResponse:
@@ -84,5 +147,11 @@ def task_delete_view(request: HttpRequest, pk: int) -> HttpResponse:
     :rtype: :class:`django.http.HttpResponse`
 
     """
+
+    for task in tasks:
+        if task["pk"] == pk:
+            break
+    else:
+        raise Http404
 
     return redirect("tasks:list")
