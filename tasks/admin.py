ce8fc0775be372c5a296a3072e6827eb781a7ccd
Serhii Horodilov <sgorodil@gmail.com>
2024-02-02 15:54:36 +0200

    Register task model to admin site (fixes GH-6)

---
+++ tasks/admin.py
@@ -0,0 +1,28 @@
+"""
+Tasks application admin site
+
+"""
+
+from django.contrib import admin
+
+from tasks.models import TaskModel
+
+
+@admin.register(TaskModel)
+class TaskModelAdmin(admin.ModelAdmin):
+    """
+    Tasks admin site configuration
+
+    """
+
+    list_display = ("summary", "get_reporter", "get_assignee")
+    list_display_links = ("summary",)
+    list_filter = ("reporter", "assignee")
+
+    @admin.display(description="reported by")
+    def get_reporter(self, obj):
+        return obj.reporter
+
+    @admin.display(description="assigned to")
+    def get_assignee(self, obj):
+        return obj.assignee
