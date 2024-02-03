6256de7dbff288d7871bea206547678811fec927
Serhii Horodilov <sgorodil@gmail.com>
2024-02-03 12:39:19 +0200

    Add auth forms drafts (GH-9)
    Apply links for sign-in/sign-up views (fixes GH-22)

---
+++ users/forms.py
@@ -0,0 +1,20 @@
+"""
+Users application forms
+
+"""
+
+from django import forms
+
+
+class SignUpForm(forms.Form):
+    """
+    User sign up (registration) form implementation
+
+    """
+
+
+class SignInForm(forms.Form):
+    """
+    User sign in (log in) form implementation
+
+    """
