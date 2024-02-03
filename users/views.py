b4633eb35283870ef7ba0cfa218619740dbfeeac
Serhii Horodilov <sgorodil@gmail.com>
2024-02-03 23:15:37 +0200

    Implement users registration, login and logout (GH-9)

--- users/views.py
+++ users/views.py
@@ -3,13 +3,19 @@ Users application views
 
 """
 
+from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.decorators.http import require_http_methods
 
 from users.forms import SignInForm, SignUpForm
 
+UserModel = get_user_model()
 
+
+@login_required(login_url=reverse_lazy("users:sign-in"))
 def user_profile_view(request: HttpRequest) -> HttpResponse:
     """
     Handle requests to user profile view
@@ -41,7 +47,10 @@ def auth_sign_up_view(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            ...
+            del form.cleaned_data["confirm_password"]
+            UserModel.objects.create_user(**form.cleaned_data)
+
+            return redirect("users:sign-in")
 
     else:
         form = SignUpForm()
@@ -65,7 +74,12 @@ def auth_sign_in_view(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = SignInForm(request.POST)
         if form.is_valid():
-            ...
+            user = UserModel.objects.get(username=form.cleaned_data["username"])
+            login(request, user)
+            # noinspection PyTypeChecker
+            url = request.GET.get("next", reverse_lazy("users:profile"))
+
+            return redirect(url)
 
     else:
         form = SignInForm()
@@ -84,5 +98,7 @@ def auth_sign_out_view(request: HttpRequest) -> HttpResponse:
     :rtype: :class:`django.http.HttpResponse`
 
     """
+
+    logout(request)
 
     return redirect("tasks:list")
