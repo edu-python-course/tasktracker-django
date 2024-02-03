ab31995b97c01559a088f6399bff9f80b7e7e2f7
Serhii Horodilov <sgorodil@gmail.com>
2024-02-03 12:42:14 +0200

    Add auth forms fields (GH-8)

--- users/forms.py
+++ users/forms.py
@@ -12,9 +12,17 @@ class SignUpForm(forms.Form):
 
     """
 
+    username = forms.CharField(max_length=64)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
 
 class SignInForm(forms.Form):
     """
     User sign in (log in) form implementation
 
     """
+
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
