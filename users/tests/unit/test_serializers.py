2d46c93ee425800492e41d8d014ecf739cfce7ec
Serhii Horodilov <sgorodil@gmail.com>
2024-02-06 21:06:36 +0200

    Update user model serializer and tests (fixes GH-35)

--- users/tests/unit/test_serializers.py
+++ users/tests/unit/test_serializers.py
@@ -18,6 +18,7 @@ class TestUserModelSerializer(test.TestCase):
             "password": "<PASSWORD>",
             "confirm_password": "<PASSWORD>",
         }
+        cls.instance = UserModel.objects.get(pk=1)  # username: "butime"
 
     def test_user_created(self):
         serializer = UserModelSerializer(data=self.data)
@@ -27,20 +28,20 @@ class TestUserModelSerializer(test.TestCase):
         qs = UserModel.objects.filter(email=self.data["email"])
         self.assertTrue(qs.exists())
 
-    def test_password_hashing_for_created_users(self):
+    def test_create_password_hashing(self):
         serializer = UserModelSerializer(data=self.data)
 
         serializer.is_valid(raise_exception=True)
         instance = serializer.save()
         self.assertTrue(instance.check_password(self.data["password"]))
 
-    def test_password_hashing_for_updated_users(self):
-        instance = UserModel.objects.get(username="butime")
-        serializer = UserModelSerializer(instance, data=self.data)
+    def test_update_password_hashing(self):
+        serializer = UserModelSerializer(self.instance, data=self.data)
 
         serializer.is_valid(raise_exception=True)
-        instance = serializer.save()
-        self.assertTrue(instance.check_password(self.data["password"]))
+        serializer.save()
+        self.instance.refresh_from_db()
+        self.assertTrue(self.instance.check_password(self.data["password"]))
 
     def test_username_validation(self):
         data = self.data.copy()
@@ -64,14 +65,23 @@ class TestUserModelSerializer(test.TestCase):
     def test_partial_update(self):
         # since validate method is modified, this behavior should be tested
         # to avoid errors
-        instance = UserModel.objects.get(username="butime")
         data = {"first_name": "Wilcome", "last_name": "Brownlock"}
         serializer = UserModelSerializer(
-            instance, data, partial=True
+            self.instance, data, partial=True
         )
 
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        instance.refresh_from_db()
-        self.assertEqual(instance.first_name, data["first_name"])
-        self.assertEqual(instance.last_name, data["last_name"])
+        self.instance.refresh_from_db()
+        self.assertEqual(self.instance.first_name, data["first_name"])
+        self.assertEqual(self.instance.last_name, data["last_name"])
+
+    def test_partial_password_update(self):
+        data = {"password": "<NEW_PASSWORD>"}  # no confirm password provided
+        serializer = UserModelSerializer(self.instance, data, partial=True)
+
+        self.assertRaises(
+            ValidationError, serializer.is_valid, raise_exception=True
+        )
+        self.assertIn("password", serializer.errors)
+        self.assertIn("confirm_password", serializer.errors)
