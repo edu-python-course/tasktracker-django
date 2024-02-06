1a4741b225d78386c4bf894424c06304ea3224ca
Serhii Horodilov <sgorodil@gmail.com>
2024-02-06 10:40:17 +0200

    Refactor sign up and sign in views with use of CBV (GH-11)

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,7 @@ from users import views
 app_name = "users"
 urlpatterns = [
     path("profile/", views.user_profile_view, name="profile"),
-    path("sign-up/", views.auth_sign_up_view, name="sign-up"),
-    path("sign-in/", views.auth_sign_in_view, name="sign-in"),
+    path("sign-up/", views.SignUpView.as_view(), name="sign-up"),
+    path("sign-in/", views.SignInView.as_view(), name="sign-in"),
     path("sign-out/", views.SignOutView.as_view(), name="sign-out"),
 ]
