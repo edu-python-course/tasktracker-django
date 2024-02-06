9e18a96fd172a060fb8c49ba352fe71b43ec029e
Serhii Horodilov <sgorodil@gmail.com>
2024-02-06 20:16:30 +0200

    Add user model serializer (GH-35)

--- users/serializers.py
+++ users/serializers.py
@@ -9,41 +9,58 @@ from rest_framework import serializers
 UserModel = get_user_model()
 
 
-class UserSerializer(serializers.Serializer):
+class UserModelSerializer(serializers.ModelSerializer):
     """
-    User serializer
+    User model serializer
+
+    This serializer is used to retrieve or update user instances.
 
     """
 
-    pk = serializers.IntegerField(
-        read_only=True
-    )
-    password = serializers.CharField(
-        max_length=128,
-        write_only=True
-    )
-    username = serializers.CharField(
-        max_length=150,
-    )
-    email = serializers.EmailField()
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(required=True)
 
-    def create(self, validated_data: dict) -> UserModel:
-        """
-        Create a model instance from validated data
+    class Meta:
+        model = UserModel
+        fields = ("pk",
+                  "username", "password", "confirm_password",
+                  "first_name", "last_name", "email", "image")
 
-        """
-
+    def create(self, validated_data):
         return UserModel.objects.create_user(**validated_data)
 
-    def update(self, instance: UserModel, validated_data: dict) -> UserModel:
-        """
-        Update a model instance from validated data
-
-        """
-
-        instance.username = validated_data["username"]
-        instance.set_password(validated_data["password"])
-        instance.email = validated_data["email"]
-        instance.save()
+    def update(self, instance, validated_data):
+        instance = super().update(instance, validated_data)
+        if "password" in validated_data:
+            instance.set_password(validated_data["password"])
 
         return instance
+
+    def validate(self, data: dict) -> dict:
+        """
+        Perform custom validations, before declared via model validators
+
+        :raise: `rest_framework.exceptions.ValidationError`
+
+        """
+
+        # validate existing usernames
+        if UserModel.objects.filter(username=data.get("username")).exists():
+            raise serializers.ValidationError({
+                "username": "username is already taken",
+            })
+
+        # validate passwords match
+        if data.get("password") != data.get("confirm_password"):
+            raise serializers.ValidationError({
+                "password": "passwords don't match",
+                "confirm_password": "passwords don't match",
+            })
+
+        # remove confirm password key
+        if "confirm_password" in data:
+            del data["confirm_password"]
+
+        return super().validate(data)
