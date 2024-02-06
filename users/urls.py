8a66f9f37c2ad673d8c2d5718d714511e61b5cea
Serhii Horodilov <sgorodil@gmail.com>
2024-02-06 10:00:03 +0200

    Refactor sign out view with use of CBV (GH-11)

--- users/urls.py
+++ users/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path("profile/", views.user_profile_view, name="profile"),
     path("sign-up/", views.auth_sign_up_view, name="sign-up"),
     path("sign-in/", views.auth_sign_in_view, name="sign-in"),
-    path("sign-out/", views.auth_sign_out_view, name="sign-out"),
+    path("sign-out/", views.SignOutView.as_view(), name="sign-out"),
 ]
