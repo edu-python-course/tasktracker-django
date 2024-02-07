a17c99eb8e941f190326d2e98b6384cbc5570dce
Serhii Horodilov <sgorodil@gmail.com>
2024-02-07 11:06:02 +0200

    Implement API resource (fixes GH-37)

--- users/routes.py
+++ users/routes.py
@@ -8,5 +8,6 @@ from django.urls import path
 from users import resources
 
 urlpatterns = [
-    path("users/", resources.users_resource),
+    path("users/", resources.users_list_resource),
+    path("users/<int:pk>/", resources.users_detail_resource),
 ]
