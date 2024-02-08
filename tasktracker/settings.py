5b1914a31697f14f533996083aa1c91caa73449e
Serhii Horodilov <sgorodil@gmail.com>
2024-02-08 17:20:14 +0200

    Create apps configs and add to installed apps list

--- tasktracker/settings.py
+++ tasktracker/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "tasks.apps.TasksAppConfig",
+    "users.apps.UsersAppConfig",
 ]
 
 MIDDLEWARE = [
