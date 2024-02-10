366d9e8e10c3a5dddb92727569cec4b866d91ced
Serhii Horodilov <sgorodil@gmail.com>
2024-02-10 19:55:15 +0200

    Alter user email field (fixes GH-54)

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ Users application models
 """
 
 from django.contrib.auth.models import AbstractUser
+from django.db.models import EmailField
 
 
 class UserModel(AbstractUser):
@@ -12,6 +13,11 @@ class UserModel(AbstractUser):
         verbose_name = "user"
         verbose_name_plural = "users"
         ordering = ("last_name", "first_name", "username")
+
+    email = EmailField(
+        unique=True,
+        verbose_name="email address",
+    )
 
     def __str__(self) -> str:
         """Return a string version of an instance"""
