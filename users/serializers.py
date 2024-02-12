8e09bbb1345cd43a90e9b141ec78203ca7e31b99
Serhii Horodilov <sgorodil@gmail.com>
2024-02-12 11:48:46 +0200

    Add base user serializer (GH-91)

---
+++ users/serializers.py
@@ -0,0 +1,26 @@
+"""
+Users application serializers
+
+"""
+
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+UserModel = get_user_model()
+
+
+class UserSerializer(serializers.Serializer):
+    """
+    User serializer
+
+    """
+
+    pk = serializers.IntegerField(read_only=True)
+
+    password = serializers.CharField(write_only=True)
+    username = serializers.CharField(max_length=150, write_only=True)
+    email = serializers.EmailField()
+
+    first_name = serializers.CharField(max_length=150, required=False)
+    last_name = serializers.CharField(max_length=150, required=False)
+    email = serializers.EmailField()
