161118d74fc347aed0f2fabe754ffc36f46996b8
Serhii Horodilov <sgorodil@gmail.com>
2024-02-13 11:24:48 +0200

    Add tasks API router

--- tasks/routes.py
+++ tasks/routes.py
@@ -3,20 +3,15 @@ Tasks application API routes
 
 """
 
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from tasks import resources
+from tasks.resources import TaskModelViewSet
+
+router = DefaultRouter()
+router.register(r"tasks", TaskModelViewSet, basename="tasks")
 
 app_name = "tasks"
 urlpatterns = [
-    path("tasks/", resources.TaskModelViewSet.as_view({
-        "get": "list",
-        "post": "create",
-    }), name="tasks-list"),
-    path("tasks/<uuid:pk>/", resources.TaskModelViewSet.as_view({
-        "get": "retrieve",
-        "put": "update",
-        "patch": "partial_update",
-        "delete": "destroy",
-    }), name="tasks-detail")
+    path("", include(router.urls)),
 ]
